5bef33a5d04f5a418ea6a3df8d1391e6db2db03b
Xoan <xoanin05@gmail.com>
2025-07-25 23:51:17 +0200

    Add files via upload

--- Proyectos_xoan/C03/ex03/ft_strncat.c
+++ Proyectos_xoan/C03/ex03/ft_strncat.c
@@ -1,31 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jumacias <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/25 22:14:37 by jumacias          #+#    #+#             */
-/*   Updated: 2025/07/25 22:22:12 by jumacias         ###   ########.fr       */
+/*   Created: 2025/07/25 23:39:15 by jumacias          #+#    #+#             */
+/*   Updated: 2025/07/25 23:43:30 by jumacias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	int	dest_len;
-	int	i;
+	unsigned int	dest_len;
+	unsigned int	i;
 
-	if (dest == NULL || src == NULL)
-	{
-		return (dest);
-	}
 	dest_len = 0;
 	while (dest[dest_len] != '\0')
 	{
 		dest_len++;
 	}
 	i = 0;
-	while (src[i] != '\0')
+	while (src[i] != '\0' && i < nb)
 	{
 		dest[dest_len + i] = src[i];
 		i++;
